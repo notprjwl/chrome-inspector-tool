1b96cf2a575feabc4c1a34bf598fd1638aa7d4e3
Prajwal S <prajwalplays@gmail.com>
2025-01-22 17:28:12 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,15 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <head>
-      <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <head>
+        <script src='https://cdn.tailwindcss.com'></script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
