7b2d617752ed0cf2e47ec7ec438f2ff061fb652b
Prajwal S <prajwalplays@gmail.com>
2025-01-22 17:33:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               <h3 className='text-lg font-semibold mb-2'>Box 1</h3>
               <p className='text-sm text-justify text-gray-500 tracking-tighter'>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                Ipsum has been the industry standard dummy text ever since the 1500s, when an
                 unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries, but also the leap into electronic
                 typesetting, remaining essentially unchanged.
@@ -31,8 +31,8 @@ export default function Home() {
               <p className='text-sm text-justify text-gray-500 tracking-tighter'>
                 It is a long established fact that a reader will be distracted by the readable
                 content of a page when looking at its layout. The point of using Lorem Ipsum is that
-                it has a more-or-less normal distribution of letters, as opposed to using 'Content
-                here, content here', making it look like readable English.
+                it has a more-or-less normal distribution of letters, as opposed to using Content
+                here, content here, making it look like readable English.
               </p>
             </div>
           </div>
