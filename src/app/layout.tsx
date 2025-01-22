dc40b4cc7972b72990685804719a30208e77332c
Prajwal S <prajwalplays@gmail.com>
2025-01-22 17:17:09 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <script src="https://cdn.tailwindcss.com"></script>
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
